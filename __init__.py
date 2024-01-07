e6d185bd399f8c5bb8869f4430efb3a5985273c1
JarbasAi <jarbasai@mailfence.com>
2024-01-07 05:33:29 +0000

    refactor/ocp

--- __init__.py
+++ __init__.py
@@ -1,57 +1,59 @@
-from os.path import join, dirname
 import random
+from os.path import join, dirname
 
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_utils.log import LOG
-from ovos_utils.parse import fuzzy_match
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
-    ocp_search, ocp_featured_media
-from youtube_archivist import YoutubeMonitor
+import requests
+from json_database import JsonStorageXDG
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
 class DeepCDigitalSkill(OVOSCommonPlaybackSkill):
-    def __init__(self):
-        super().__init__("DeepCDigital")
-        self.supported_media = [MediaType.MOVIE,
-                                MediaType.GENERIC]
+    def __init__(self, *args, **kwargs):
+        self.supported_media = [MediaType.MOVIE]
         self.skill_icon = self.default_bg = join(dirname(__file__), "ui", "deepcdigital_icon.jpg")
-        self.archive = YoutubeMonitor(db_name="DeepCDigitalMovies",
-                                      min_duration=30 * 60,
-                                      logger=LOG,
-                                      blacklisted_kwords=["trailer", "teaser", "movie scene",
-                                                          "movie clip", "behind the scenes",
-                                                          "Movie Preview", "soundtrack", " OST", "opening theme"])
+        self.archive = JsonStorageXDG("DeepCDigitalMovies", subfolder="OCP")
+        super().__init__(*args, **kwargs)
 
     def initialize(self):
-        bootstrap = "https://github.com/JarbasSkills/skill-deepcdigital/raw/dev/bootstrap.json"
-        self.archive.bootstrap_from_url(bootstrap)
-        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+        self._sync_db()
+        self.load_ocp_keywords()
 
     def _sync_db(self):
-        url = "https://www.youtube.com/channel/UC-b1LXc71wj0rMVQOWw-UfQ"
-        self.archive.parse_videos(url)
-        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
+        bootstrap = "https://github.com/JarbasSkills/skill-deepcdigital/raw/dev/bootstrap.json"
+        data = requests.get(bootstrap).json()
+        self.archive.merge(data)
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+        self.media_type_exceptions = {}
 
-    # matching
-    def match_skill(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "movie") or media_type == MediaType.MOVIE:
-            score += 40
-        return score
+    def load_ocp_keywords(self):
+        title = []
+        docus = []
 
-    def normalize_title(self, title):
-        title = title.lower().strip()
-        title = self.remove_voc(title, "movie")
-        title = title.replace("|", "").replace('"', "") \
-            .replace(':', "").replace('”', "").replace('“', "") \
-            .strip()
-        return " ".join(
-            [w for w in title.split(" ") if w])  # remove extra spaces
+        for url, data in self.archive.items():
+            t = data["title"].split("|")[0].split("(")[0].strip().strip("¿?.!").rstrip("¿")
+            if "documentary" in data["title"].lower():
+                print(t)
+                docus.append(t)
+                # signal this entry as DOCUMENTARY media type
+                # in case it gets selected later
+                self.media_type_exceptions[data["url"]] = MediaType.DOCUMENTARY
+            else:
+                title.append(t)
+                if ":" in t:
+                    t1, t2 = t.split(":", 1)
+                    title.append(t1.strip())
+                    title.append(t2.strip())
 
-    def calc_score(self, phrase, match, base_score=0):
-        score = base_score
-        score += 100 * fuzzy_match(phrase.lower(), match["title"].lower())
-        return min(100, score)
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_name", title)
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_name", docus)
+        self.register_ocp_keyword(MediaType.MOVIE,
+                                  "movie_streaming_provider",
+                                  ["Deep C Digital",
+                                   "DeepCDigital"])
 
     def get_playlist(self, score=50, num_entries=250):
         pl = self.featured_media()[:num_entries]
@@ -69,25 +71,62 @@ class DeepCDigitalSkill(OVOSCommonPlaybackSkill):
 
     @ocp_search()
     def search_db(self, phrase, media_type):
-        base_score = self.match_skill(phrase, media_type)
-        if self.voc_match(phrase, "deepcdigital"):
-            yield self.get_playlist(base_score)
-        if media_type == MediaType.MOVIE:
-            # only search db if user explicitly requested movies
-            phrase = self.normalize_title(phrase)
-            for url, video in self.archive.db.items():
+        base_score = 15 if media_type == MediaType.MOVIE else 0
+        entities = self.ocp_voc_match(phrase)
+
+        skill = "movie_streaming_provider" in entities  # skill matched
+
+        base_score += 30 * len(entities)
+        title = entities.get("movie_name")
+        dtitle = entities.get("documentary_name")
+
+        if media_type == MediaType.DOCUMENTARY:
+            candidates = [video for video in self.archive.values()
+                          if self.media_type_exceptions.get(video["url"], MediaType.MOVIE) ==
+                          MediaType.DOCUMENTARY]
+
+        else:
+            candidates = [video for video in self.archive.values()
+                          if video["url"] not in self.media_type_exceptions]
+
+        if title:
+            base_score += 30
+            candidates = [video for video in candidates
+                          if title.lower() in video["title"].lower()]
+            for video in candidates:
                 yield {
                     "title": video["title"],
-                    "author": "Full Free Films",
-                    "match_confidence": self.calc_score(phrase, video, base_score),
+                    "author": video["author"],
+                    "match_confidence": min(100, base_score),
                     "media_type": MediaType.MOVIE,
-                    "uri": "youtube//" + url,
+                    "uri": "youtube//" + video["url"],
                     "playback": PlaybackType.VIDEO,
                     "skill_icon": self.skill_icon,
                     "skill_id": self.skill_id,
                     "image": video["thumbnail"],
-                    "bg_image": self.default_bg
+                    "bg_image": video["thumbnail"]
                 }
+
+        if dtitle:
+            base_score += 20
+            candidates = [video for video in candidates
+                          if dtitle.lower() in video["title"].lower()]
+            for video in candidates:
+                yield {
+                    "title": video["title"],
+                    "author": video["author"],
+                    "match_confidence": min(100, base_score),
+                    "media_type": MediaType.DOCUMENTARY,
+                    "uri": "youtube//" + video["url"],
+                    "playback": PlaybackType.VIDEO,
+                    "skill_icon": self.skill_icon,
+                    "skill_id": self.skill_id,
+                    "image": video["thumbnail"],
+                    "bg_image": video["thumbnail"]
+                }
+
+        if skill:
+            yield self.get_playlist()
 
     @ocp_featured_media()
     def featured_media(self):
@@ -101,8 +140,16 @@ class DeepCDigitalSkill(OVOSCommonPlaybackSkill):
             "skill_icon": self.skill_icon,
             "bg_image": video["thumbnail"],
             "skill_id": self.skill_id
-        } for video in self.archive.sorted_entries()]
+        } for video in self.archive.values()]
 
 
-def create_skill():
-    return DeepCDigitalSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = DeepCDigitalSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_db("play Homecoming Massacre", MediaType.MOVIE):
+        print(r)
+        # {'title': 'Homecoming Massacre | Full Horror Movie | Kenny White | Killer Clown', 'author': 'Deep C Digital', 'match_confidence': 75, 'media_type': <MediaType.MOVIE: 10>, 'uri': 'youtube//https://youtube.com/watch?v=mwOx2kgG26c', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake', 'image': 'https://i.ytimg.com/vi/mwOx2kgG26c/sddefault.jpg?v=62f2bd45', 'bg_image': 'https://i.ytimg.com/vi/mwOx2kgG26c/sddefault.jpg?v=62f2bd45'}
+    for r in s.search_db("play Is Genesis History", MediaType.DOCUMENTARY):
+        print(r)
+        # {'title': 'Is Genesis History? (2017) | Documentary | Del Tackett, Kevin L. Anderson, Steve A. Austin', 'author': 'Deep C Digital', 'match_confidence': 50, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'youtube//https://youtube.com/watch?v=ulpQSgm70J4', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake', 'image': 'https://i.ytimg.com/vi/ulpQSgm70J4/sddefault.jpg?v=63b71431', 'bg_image': 'https://i.ytimg.com/vi/ulpQSgm70J4/sddefault.jpg?v=63b71431'}
